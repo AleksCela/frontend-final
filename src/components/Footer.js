31d4db8a9f06f5e2948e4858b56d3339d01d7924
fjoralba29 <90421671+fjoralba29@users.noreply.github.com>
2022-12-22 00:30:10 +0100

    Style Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="footer py-2 bg-dark bg-gradient">
+        <footer className="footer py-2 bg-dark bg-gradient fixed-bottom">
             <div className="container text-center">
                 <span className="text-muted">My trips diary</span>
             </div>
