d2d738f8b918dbf300b70878374a991b9e37ffe8
fjoralba29 <90421671+fjoralba29@users.noreply.github.com>
2022-12-22 00:31:12 +0100

    Fixed function & style

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import NavbarSignupLogin from '../components/NavbarSignupLogin.js';
+import Footer from "../components/Footer.js";
 
 export default function Login() {
 
@@ -35,27 +37,34 @@ export default function Login() {
   },)
 
   return (
-    <form id="loginForm" onSubmit={handleSubmit}>
-      <div className="form-outline mb-4">
-        <input onChange={(event) =>
-          setEmail(event.target.value)
-        } type="text" id="form3Example1cg" name="email" className="form-control form-control-lg" />
-        <label className="form-label" htmlFor="form3Example1cg">email</label>
+    
+    <div className="bg-light w-100 h-100 position-absolute d-inline-block ">
+      <NavbarSignupLogin />
+      <div className="container-md bg-white rounded-3 my-5 p-4">
+        <h1 className="text-center">Log In</h1>      
+        <form class="form-horizontal row g-2" id="loginForm" onSubmit={handleSubmit}>
+          <div className="col-10 offset-1 col-lg-4 offset-lg-4 div-wrapper">
+            <label className="form-label" htmlFor="form3Example1cg">Email</label>
+            <input onChange={(event) =>
+              setEmail(event.target.value)
+            } type="text" id="form3Example1cg" name="email" className="form-control form-control-lg" /> 
+          </div>
+          <div className="col-10 offset-1 col-lg-4 offset-lg-4 div-wrapper">
+            <label className="form-label" htmlFor="form3Example4cg">Password</label>
+            <input onChange={(event) =>
+              setPassword(event.target.value)
+            } type="password" name="password" id="form3Example4cg" className="form-control form-control-lg" />
+            {error ? <p id="invalidPassword" className="invalidPassword">Password and email do not
+              match!</p> : null}
+          </div>
+          <div className="d-flex justify-content-center">
+            <button type="submit" className="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Log
+              In</button>
+          </div>
+          <p className="text-center text-muted mt-5 mb-0">Dont have an account?<Link to={"/sign-up"}>Sign Up</Link></p>
+        </form>
       </div>
-      <div className="form-outline mb-4">
-        <input onChange={(event) =>
-          setPassword(event.target.value)
-        } type="password" name="password" id="form3Example4cg" className="form-control form-control-lg" />
-        <label className="form-label" htmlFor="form3Example4cg">Password</label>
-        {error ? <p id="invalidPassword" className="invalidPassword">Password and email do not
-          match!</p> : null}
-      </div>
-      <div className="d-flex justify-content-center">
-        <button type="submit" className="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Log
-          In</button>
-      </div>
-      <p className="text-center text-muted mt-5 mb-0">Dont have an account?<Link to={"/sign-up"}>Sign Up</Link></p>
-    </form>
-
+      <Footer />
+    </div>
   );
 }
