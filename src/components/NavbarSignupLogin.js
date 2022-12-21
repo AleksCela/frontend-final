75655cf0e2151a2ebb94042b5452a164bf7b6e69
fjoralba29 <90421671+fjoralba29@users.noreply.github.com>
2022-12-22 00:30:43 +0100

    Navbar for Signup & Login

---
+++ src/components/NavbarSignupLogin.js
@@ -0,0 +1,11 @@
+import React, { } from 'react';
+
+export default function Navbar() {
+    return (
+        <nav class="navbar navbar-expand-lg bg-white">
+            <div class="container-fluid">
+                <a class="navbar-brand"><b>My trips</b> diary</a>                
+            </div>
+        </nav>
+    )
+}
